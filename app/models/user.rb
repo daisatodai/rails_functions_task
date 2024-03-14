bfbdc05bed3fc92ca438f9ee90eabd5900a08aac
dai <sugerbig15@gmail.com>
2024-03-14 16:10:36 +0900

    firstcommit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
+  has_one_attached :profile_image
   validates :password, length: { minimum: 6 }
 end
