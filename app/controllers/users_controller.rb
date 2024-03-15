25865781812bc7411677195f4419926e1e2c37a7
dai <sugerbig15@gmail.com>
2024-03-15 11:32:26 +0900

    laterに修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.with(to: "sample@gmail.com", name: "山田 太郎").welcome.deliver_later
+      UserMailer.with(to: @user.email, name: @user.name).welcome.deliver_later
       log_in(@user)
       redirect_to user_path(@user.id), notice: 'アカウントを登録しました。'
     else
